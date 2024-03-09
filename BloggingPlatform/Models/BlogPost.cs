fd4d800934d8f2e1b231f12a34417373c4d59ad0
Eryk <01168936@pw.edu.pl>
2024-03-09 18:50:39 +0100

    User and BlogPost classes initialized

---
+++ BloggingPlatform/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace BloggingPlatform.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }
+        public string Content { get; set; } 
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ BloggingPlatform/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BloggingPlatform.Models
+{
+    public class User
+    { 
+        public int Id { get; set; }  
+        public string UserName { get; set; } 
+        public string Email { get; set; }
+        public ICollection<BlogPost>? Posts { get; set; }
+    }
+}
